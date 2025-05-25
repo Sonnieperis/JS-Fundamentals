06514bdc58b657f14cf36788d617993de34c4efa
Muthoni(sonnieperis) <sonnieperis17@gmail.com>
2025-05-25 19:30:56 +0300

    Add script to print two arguments in 'arg1 is arg2' format

---
+++ 4-concat.js
@@ -0,0 +1,6 @@
+// Access command line arguments from process.argv
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+// Print the formatted output using console.log
+console.log(`${arg1} is ${arg2}`);
