ea4f3687846009dcba7e492f3451aaf917c8a278
Muthoni(sonnieperis) <sonnieperis17@gmail.com>
2025-05-25 19:46:38 +0300

    Add script to print 'C is fun' x times

---
+++ 7-multi_c.js
@@ -0,0 +1,10 @@
+const arg = process.argv[2];
+const x = parseInt(arg);
+
+if (isNaN(x) || x <= 0) {
+  console.log("Missing number of occurrences");
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log("C is fun");
+  }
+}
